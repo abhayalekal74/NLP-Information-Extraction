a72108d8f8bf75090fc2e5b66246262ad0bbf9fd
Abhay Alekal <abhayalekal74@gmail.com>
2019-02-12 16:20:31 +0000

    Data extracted

--- extract.py
+++ extract.py
@@ -35,11 +35,30 @@ def is_rounding_section_in_page(page_content):
 
 
 def chunk(tags):
-
-    amount_types = []
-    currencies = []
+    amount_types = ['', '']
+    currencies = ['', '']
     roundings = ['nearest', 'nearest']
-    amount_values = []
+    amount_values = ['', '']
+
+    currency_regex = re.compile(r'\b[A-Z]{3}\b') 
+
+    def get_currency_indices(words, tags):
+        indices = [i for i, x in enumerate(tags) if x == 'CD']
+        # Remove indices which do not have currency as their previous word
+        for i in range(len(indices)):
+            if not currency_regex.search(words[indices[i] - 1]):
+                del indices[i]        
+        return indices
+
+    def get_roundings(words, tags):
+        try:
+            # If index throws an error or the if condition fails, rounding info is not given
+            _vbn_index = tags.index('VBN') 
+            if _vbn_index < len(tags) - 1 and words[_vbn_index] == 'rounded' and words[_vbn_index + 1] in ['up', 'down']:
+                return _vbn_index, True
+        except ValueError:
+            pass
+        return -1, False
 
     def parse_together_match(words, tags):
         # Check if Delivery and Return are present
@@ -50,43 +69,57 @@ def chunk(tags):
             # False positive
             return False
 
+        # Storing data in the order of occurence
         _del_data_index = 0 if _del_word_index < _ret_word_index else 1
         _ret_data_index = (_del_data_index + 1) % 2
 
         amount_types[_del_data_index] = 'Delivery Amount'
         amount_types[_ret_data_index] = 'Return Amount'
     
-        try:
-            # If index throws an error or the if condition fails, rounding info is not given
-            _vbn_index = tags.index('VBN') 
-            if _vbn_index < len(tags) - 1 and words[vbn_index + 1] in ['up', 'down']:
-                # Its given whether the values are rounded up or down
-                roundings[0] = words[vbn_index + 1]
+        # If index throws an error or the if condition fails, rounding info is not given
+        _vbn_index, has_roundings = get_roundings(words, tags)
+        if has_roundings: 
+            # Its given whether the values are rounded up or down
+            roundings[0] = words[_vbn_index + 1]
 
-                # Check within the next 3 words if there's another rounding 
-                for i in range(_vbn_index + 2, min(len(words), _vbn_index + 5)):
-                    if words[i] in ['up', 'down']:
-                        roundings[1] = words[i]
-                        break
-                if roundings[1] == 'nearest': # For two amounts only one rounding is given, so it must be the same for both
-                    roundings[1] = roundings[0]
-        except ValueError:
-            pass
+            # Check within the next 3 words if there's another rounding 
+            for i in range(_vbn_index + 2, min(len(words), _vbn_index + 5)):
+                if words[i] in ['up', 'down']:
+                    roundings[1] = words[i]
+                    break # Rounding found, break
+            if roundings[1] == 'nearest': # For two amounts only one rounding is given, so it must be the same for both
+                roundings[1] = roundings[0]
 
-        indices = [i for i, x in enumerate(tags) if x == 'CD']
-        currency_regex = re.compile(r'\b[A-Z]{3}\b') 
-        if len(indices) > 2:
+        indices = get_currency_indices(words, tags)
+        amount_values[0] = words[indices[0]]
+        currencies[0] = words[indices[0] - 1]
+        if len(indices) > 1:
+            # If both prices are mentioned separately
+            amount_values[1] = words[indices[1]]
+            currencies[1] = words[indices[1] - 1]
+        else:
+            # If the prices are the same
+            amount_values[1] = amount_values[0]
+            currencies[1] = currencies[0]
+        return True
             
-
     def parse_separate_match(words, tags):
-        print (words, tags)
         if ('Delivery' in words or 'Return' in words) and 'Amount' in words:
-            pass
+            if len(amount_types[0]) == 0: # Setting amount_types once
+                amount_types[0] = 'Delivery Amount'
+                amount_types[1] = 'Return Amount'
+            active_data_index = 0 if 'Delivery' in words and 'Amount' in words else 1 # Tells whether working with Delivery Amount or Return Amount 
+            indices = get_currency_indices(words, tags)
+            amount_values[active_data_index] = words[indices[0]]
+            currencies[active_data_index] = words[indices[0] - 1]
+            _vbn_index, has_roundings = get_roundings(words, tags)
+            roundings[active_data_index] = words[_vbn_index + 1] if has_roundings else 'nearest'
 
     grammar = ('''
                TOGETHER: {<DT>?<NNP>{2}<.*>{0,10}<CC><.*>{0,10}<DT>?<NNP>{2}<.*>{0,5}?(<VBN>(<RP>|<RB>)(<.*>{0,2}(<RP>|<RB>)?))?<.*>{0,10}<NNP><CD>(<.*>{0,4}?<NNP><CD>)?}
-               SEPARATE: {<DT>?<NNP>{2}<.*>{0,10}?(<VBN>(<RP>|<RB>))?<.*>{0,10}?<NNP><CD>(<.*>{0,4}?<NNP><CD>)?}  
+               SEPARATE: {<DT>?<NNP>{2}<.*>{0,10}?(<VBN>(<RP>|<RB>))?<.*>{0,10}?<NNP><CD>}  
                ''')
+
     chunk_regex_parser = nltk.RegexpParser(grammar)
     parsed_tree = chunk_regex_parser.parse(tags)
     subtree_matches = parsed_tree.subtrees(filter=lambda x: x.label() in ['TOGETHER', 'SEPARATE'])
@@ -103,7 +136,10 @@ def chunk(tags):
             if data_found:
                 break
         else:
-            parse_separate_match(words, tags) # Delivery and Return amounts are separate, so letting the loop run for all subtrees
+            parse_separate_match(words, tags) # Delivery and Return amounts are separate, so letting the loop run for all subtrees, check if delivery and return data is found and stop the loop. Skipping for now
+        
+    for i in range(2):
+        print ("{}: Currency: {} Amount: {} Rounding: {}".format(amount_types[i], currencies[i], amount_values[i], roundings[i]))
 
 
 def pos_tag(pages):
@@ -123,7 +159,6 @@ def read_pdf(pdf_file):
     pagewise_content = [page_content for page_content in file_content.split('\n\n\n\n') if len(page_content) > 0]
     
     num_pages = len(pagewise_content)
-    print (num_pages)
 
     def update_pointers(dir_flag, fwd_ptr, back_ptr):
         if not dir_flag and fwd_ptr >= num_pages:
@@ -146,7 +181,6 @@ def read_pdf(pdf_file):
     
     while(forward_pointer < num_pages or backward_pointer >= 0):
         pointer = forward_pointer if read_direction_forward else backward_pointer # Search a page forward and backward around q3
-        print ("Checking page", pointer)
         cur_page_content = pagewise_content[pointer] 
         rounding_section_present, continued_in_next_page = is_rounding_section_in_page(cur_page_content)
         if rounding_section_present: # If rounding section is found, POS tag page
